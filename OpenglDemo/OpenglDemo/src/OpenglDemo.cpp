d52176f20bbf773fe9bd3dbaf14041629004974f
ChongJiao <840436263@qq.com>
2020-02-25 21:04:27 +0800

    add imgui library

--- OpenglDemo/OpenglDemo/src/OpenglDemo.cpp
+++ OpenglDemo/OpenglDemo/src/OpenglDemo.cpp
@@ -17,6 +17,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -30,7 +34,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -41,16 +45,32 @@ int main(void)
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
 	if (glewInit() != GLEW_OK)
 		std::cout << "GLEW ERROR" << std::endl;
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	float positions[] = {
-		-0.5f, -0.5f, 0.0f, 0.0f, 
-		 0.5f, -0.5f, 1.0f, 0.0f,
-		 0.5f,  0.5f, 1.0f, 1.0f,
-		-0.5f,  0.5f, 0.0f, 1.0f
+		100.0f, 100.0f, 0.0f, 0.0f, 
+		200.0f, 100.0f, 1.0f, 0.0f,
+		200.0f, 200.0f, 1.0f, 1.0f,
+		100.0f, 200.0f, 0.0f, 1.0f
 	};
 
 	unsigned int indices[] = {
@@ -81,10 +101,9 @@ int main(void)
 	shader.SetUniform1i("u_Texture", 0); 
 
 
-	glm::mat4 prj = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, -1.0f, 1.0f);
-	shader.SetUniformMat4f("u_MVP", prj);
+	glm::mat4 prj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
 
-	
 	va.Unbind();
 	vb.Unbind();
 	ib.Unbind();
@@ -92,13 +111,42 @@ int main(void)
 
 	Renderer render;
 
-	float r = 0.0f;
-	float increment = 0.05f;
 	/* Loop until the user closes the window */
+
+	glm::vec3 translation = glm::vec3(200, 200, 0);
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
-		render.Clear();
+
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+		glm::mat4 mvp = prj * view * model;
+		shader.SetUniformMat4f("u_MVP", mvp);
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+
+			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::End();
+		}
+		/* Render here */	
+		ImGui::Render();
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		//render.Clear();
 
 		shader.Bind();
 		texture.Bind();
@@ -107,25 +155,18 @@ int main(void)
 		//texture.Bind();
 		//shader.SetUniform1i("u_Texture", 0);
 
-		render.Draw(va, ib, shader); 
-
-		if (r > 1.0f)
-			increment = -0.05f;
-		else if (r < 0.0f)
-			increment = 0.05f;
-
-		r += increment;
-
+		render.Draw(va, ib, shader);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
 	}
 	
-	//glDeleteShader(shader);
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # OpenGL
 opengl tutorial
+
 Series tutorial from: [The cherno](https://www.youtube.com/user/TheChernoProject/playlists)
 
-upto course 20
+## plan process
+add [ImGui](https://github.com/ocornut/imgui) into opengl3 
+
+
+## next course
+upto course 23, rendering multiple objects
+
+
+
+
+
