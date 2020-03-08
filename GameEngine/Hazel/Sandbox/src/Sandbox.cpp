744bef510009da771eb41660151cc5d867e03841
ChongJiao <840436263@qq.com>
2020-03-08 20:33:48 +0800

    ???

--- GameEngine/Hazel/Sandbox/src/Sandbox.cpp
+++ GameEngine/Hazel/Sandbox/src/Sandbox.cpp
@@ -1,11 +1,31 @@
 #include "Hazel.h"
 
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{}
+
+	void OnUpdate() override
+	{
+		HZ_INFO("ExampleLayer::Update");
+	}
+
+	void OnEvent(Hazel::Event& event) 
+	{
+		HZ_TRACE("Event");
+	}
+};
+
+
+
 class Sandbox : public Hazel::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	};
 	~Sandbox()
 	{
