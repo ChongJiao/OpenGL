b53dc0e796fa859e7345e35335faa5b7966542f2
ChongJiao <840436263@qq.com>
2020-03-10 09:28:15 +0800

    Input pulling

---
+++ GameEngine/Hazel/Hazel/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,28 @@
+#include "hzpch.h"
+#include "WindowsInput.h"
+
+#include "Hazel/Application.h"
+#include <GLFW/glfw3.h>
+namespace Hazel
+{
+	Input* Input::s_Instance = new WindowsInput();
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		return std::pair<float, float>(xpos, ypos);
+	}
+}
